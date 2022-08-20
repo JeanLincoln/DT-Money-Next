9b225cd1b7164f80a2e1800860c0d62cd6c84a7e
Jean Lincoln <jeanlincol@gmail.com>
2022-08-20 10:15:31 -0700

    Home component table bugfix

--- src/pages/HomeStyles.ts
+++ src/pages/HomeStyles.ts
@@ -1,10 +1,6 @@
 import styled from "styled-components";
 
 export const TransactionsTable = styled.table`
-    display: flex;
-    flex-direction: column;
-    gap:0.8rem;
-
     border-collapse: separate;
     border-spacing: 0 0.5rem;
 
@@ -12,4 +8,37 @@ export const TransactionsTable = styled.table`
     max-width: 1120px;
 
     margin: 2.4rem  auto 0 auto;
+
+    tbody{
+        display: flex;
+        flex-direction: column;
+        gap:0.8rem;
+    }
+`
+
+export const TransactionRow = styled.tr`
+    display: flex;
+    justify-content: space-between;
+    padding: 2rem 3.2rem;
+    border-radius: 6px;
+    font-size: 1.6rem;
+
+    background-color: ${({theme})=> theme['gray-500']};  
+    td {
+        flex:1;
+
+        &:first-child {
+            flex:3; 
+        }
+    }
+`
+
+type PriceHighLightProps = {
+    variant:'income' | 'outcome'
+}
+
+export const PriceHighLight = styled.span<PriceHighLightProps>`
+    color:${(props) => props.variant === 'income'
+        ?props.theme['green-400']
+        :props.theme['red-400']};
 `
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import * as S from './HomeStyles'
 import { SeachForm } from "../components/SearchForm";
 import { Summary } from "../components/Summary";
-import { Transaction } from "../components/Transaction";
 
 export default function Home(){
   return(
@@ -9,7 +8,28 @@ export default function Home(){
       <Summary />
       <SeachForm />
       <S.TransactionsTable>
-        <Transaction/>
+        <tbody>
+          <S.TransactionRow>
+                        <td>Desenvolvimento de site</td>
+                        <td>
+                            <S.PriceHighLight variant="income">
+                                R$ 12.000.00
+                            </S.PriceHighLight>
+                        </td>
+                        <td>Trabalho</td>
+                        <td>13/04/2022</td>
+            </S.TransactionRow>
+            <S.TransactionRow>
+                        <td>Hamburger</td>
+                        <td>
+                            <S.PriceHighLight variant="outcome">
+                                -R$ 50.00
+                            </S.PriceHighLight>
+                        </td>
+                        <td>Alimentação</td>
+                        <td>13/04/2022</td>
+            </S.TransactionRow>
+          </tbody>
       </S.TransactionsTable>
     </>
   )
