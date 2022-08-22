ded22194a85afd39b4f65816b5e8abd35cdbe543
Jean Lincoln <jeanlincol@gmail.com>
2022-08-22 03:35:35 -0700

    SearchForm, Search Transactions logic added

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -1,11 +1,32 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { MagnifyingGlass } from 'phosphor-react'
+import { useContext } from 'react'
+import { useForm } from 'react-hook-form'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
+import * as z from 'zod'
 import * as S from './styles'
 
+const SearchTransactionSchema = z.object({
+    searchDescription: z.string()
+})
+
+type SearchTransactionFormInputs = z.infer<typeof SearchTransactionSchema>
+
 export function SeachForm(){
+    const { fetchTransactions } = useContext(TransactionsContext)
+    const { register, handleSubmit, reset } = useForm<SearchTransactionFormInputs>({
+        resolver: zodResolver(SearchTransactionSchema)
+    })
+
+    async function handleSearchTransactions(data:SearchTransactionFormInputs){
+        fetchTransactions(data.searchDescription)
+        reset()
+    }
+
     return(
         <S.SearchFormContainer>
-            <form >
-                <input type="text" placeholder='Busque uma transação'/>
+            <form onSubmit={handleSubmit(handleSearchTransactions)}>
+                <input {...register('searchDescription')} type="text" placeholder='Busque uma transação'/>
                 <button type='submit'>
                     <MagnifyingGlass size={20} />
                     <strong>Buscar</strong>
